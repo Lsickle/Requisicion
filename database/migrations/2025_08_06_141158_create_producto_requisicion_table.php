2a49cbd535f7d8f3061c60334e3358a0c54a9a2a
xardo <pardomoyasegio@gmail.com>
2025-09-04 13:27:04 -0500

    correction migrate stock

--- database/migrations/2025_08_06_141158_create_producto_requisicion_table.php
+++ database/migrations/2025_08_06_141158_create_producto_requisicion_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('id_producto')->constrained('productos')->onDelete('cascade'); #id del producto
             $table->foreignId('id_requisicion')->constrained('requisicion')->onDelete('cascade'); #id de la requisicion
+            $table->string('stock_e')->nullable(); #salido de stock
             $table->integer('pr_amount'); #cantidad de productos por requisicion
             $table->timestamps();
             $table->softDeletes();
